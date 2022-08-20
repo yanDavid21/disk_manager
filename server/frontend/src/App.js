6de3aef5e84f9956cf79102aa54a87a540a814dc
yanDavid21 <yanDavid21@users.noreply.github.com>
2022-08-20 03:26:51 -0400

    Updated UI with text formatting, first impl of color coding

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -8,12 +8,80 @@ import Paper from "@mui/material/Paper";
 
 const BACKEND_URL = "http://localhost:8080/api/data";
 
+const getSizeWithSuffix = (size) => {
+  if (size < 1024 * 1024) {
+    return `${(size / 1024).toFixed(2)}KB`;
+  } else if (size < 1024 * 1024 * 1024) {
+    return `${(size / (1024 * 1024)).toFixed(2)}MB`;
+  } else if (size < 1024 * 1024 * 1024 * 1024) {
+    return `${(size / (1024 * 1024 * 1024)).toFixed(2)}GB`;
+  }
+};
+
+const getInterpolatedColoringFromSize = (min, max, size) => {
+  const percentTowardsRed = (size - min) / (max - min);
+  const redValue = percentTowardsRed * 255;
+
+  return `rgb(${redValue}, 0, ${255 - redValue})`;
+};
+
+const getNameStructWithColors = (nameStruct, directoryStruct, rootDir) => {
+  if (!(nameStruct && directoryStruct && rootDir)) {
+    return nameStruct;
+  }
+
+  const nameStructWithColors = { [rootDir]: nameStruct[rootDir] };
+  console.log("test");
+  console.log(nameStructWithColors);
+  addChildrenNameStructWithColors(
+    nameStructWithColors,
+    nameStruct,
+    nameStruct[rootDir],
+    directoryStruct
+  );
+};
+
+const addChildrenNameStructWithColors = (
+  newNameStruct,
+  oldNameStruct,
+  parentDir,
+  directoryStruct
+) => {
+  const childrenFolderSizes = parentDir.subFolders.map(
+    (subFolder) => directoryStruct[subFolder]
+  );
+  const min = Math.min(...childrenFolderSizes);
+  const max = Math.max(...childrenFolderSizes);
+
+  parentDir.subFolders.forEach((subFolder) => {
+    newNameStruct[subFolder] = {
+      ...oldNameStruct[subFolder],
+      backgroundColor: getInterpolatedColoringFromSize(
+        min,
+        max,
+        directoryStruct[subFolder].size
+      ),
+    };
+    addChildrenNameStructWithColors(
+      newNameStruct,
+      oldNameStruct,
+      oldNameStruct[subFolder],
+      directoryStruct
+    );
+  });
+};
+
 const DirectoryStats = (selectedFolder) => {
+  const folder = selectedFolder.folder;
   return (
     <>
-      {Object.keys(selectedFolder.folder).map((key) => {
-        return <p key={key}>{`${key}: ${selectedFolder.folder[key]}`}</p>;
-      })}
+      <p>{`Created on: ${new Date(folder.birthTime).toLocaleString()}`}</p>
+      <p>{`Last modified on: ${new Date(
+        folder.lastModifiedTime
+      ).toLocaleString()}`}</p>
+      <p>{`Size: ${getSizeWithSuffix(folder.size)}`}</p>
+      <p>{`Number of files: ${folder.numFiles}`}</p>
+      <p>{`Number of subdirectories: ${folder.numDirs}`}</p>
     </>
   );
 };
@@ -32,7 +100,8 @@ const App = () => {
   const [directoryDict, setDirectoryDict] = useState({}); //dictionary mapping folder name to folder stats
   const [nameStruct, setNameStruct] = useState(null); //nested tree structure representing the relationship between folders (stores folder names)
   const [exploredFolders, setExploredFolders] = useState(new Set()); //tracks which folders have been opened aka. its data has been fetched
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); // loading state
+  const [folderName, setFolderName] = useState(null); //selected folder name to be displayed
 
   const fetchInitData = () => {
     fetch(BACKEND_URL)
@@ -51,6 +120,7 @@ const App = () => {
       });
   };
 
+  // set up socket connection, data will be pushed when done calculating
   const initSocketConnection = () => {
     const ws = new WebSocket("ws://localhost:8080");
     ws.onopen = (event) => {
@@ -84,9 +154,12 @@ const App = () => {
       setSelectedFolder(directoryDict[rootDir]);
     }
   }, [loading, directoryDict, rootDir]);
+  // useEffect(() => {
+  //   setNameStruct(getNameStructWithColors(nameStruct, directoryDict, rootDir));
+  // }, [directoryDict, nameStruct, rootDir]);
 
   const convertStructToTreeItem = (nameStruct, directoryPath) => {
-    const { name, subFolders } = nameStruct[directoryPath];
+    const { name, subFolders, backgroundColor } = nameStruct[directoryPath];
 
     //if the current folder has never been expanded, when clicked for the first time,
     const fetchNewNames = () => {
@@ -115,6 +188,10 @@ const App = () => {
         nodeId={directoryPath}
         key={directoryPath}
         onClick={fetchNewNames}
+        sx={{
+          backgroundColor: backgroundColor ?? "#FFFFFF",
+          color: backgroundColor ? "white" : "black",
+        }}
       >
         {subFolders.map((subFolder) =>
           convertStructToTreeItem(nameStruct, subFolder)
@@ -124,7 +201,7 @@ const App = () => {
   };
 
   return (
-    <Grid container className="App" spacing={2}>
+    <Grid container className="App" spacing={2} sx={{ padding: 20 }}>
       <Grid item xs={6}>
         <div>
           <TreeView
@@ -133,6 +210,21 @@ const App = () => {
             defaultExpandIcon={<ChevronRightIcon />}
             onNodeSelect={(event, nodeId) => {
               setSelectedFolder(directoryDict[nodeId]);
+              const withoutEndSlash = nodeId.substring(0, nodeId.length - 1);
+              const rootFolderRelPath = nodeId.substring(
+                withoutEndSlash.lastIndexOf("\\") + 1
+              );
+              const relFolderPath = nodeId.substring(
+                nodeId.lastIndexOf("\\") + 1
+              );
+
+              if (folderName) {
+                setFolderName(
+                  nodeId === rootDir ? rootFolderRelPath : relFolderPath
+                );
+              } else {
+                setFolderName(rootFolderRelPath);
+              }
             }}
           >
             {nameStruct && convertStructToTreeItem(nameStruct, rootDir)}
@@ -144,20 +236,19 @@ const App = () => {
           style={{
             height: "100vh",
             width: "100%",
+            padding: "2em",
           }}
         >
           <Paper
             variant="outlined"
             sx={{
-              height: "50vh",
               padding: "1em",
-              top: "25%",
-              position: "fixed",
-              width: "100%",
               boxSizing: "border-box",
             }}
           >
-            <Typography variant="h4">Folder Information</Typography>
+            <Typography variant="h4">
+              {folderName ?? `Folder Information`}
+            </Typography>
             {loading ? (
               <CircularProgress />
             ) : (
